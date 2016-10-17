fc8f2080e0cbff311351265a07553e5bd0b6e189
Marcos Hartmann <marcos.hartmann@litecode.com.br>
2016-10-17 12:03:19 -0200

    product list using product obj

--- src/br/com/pedido/controller/ProductRequestController.java
+++ src/br/com/pedido/controller/ProductRequestController.java
@@ -2,11 +2,11 @@ package br.com.pedido.controller;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 
+import br.com.pedido.entity.Product;
 import br.com.pedido.service.ProductService;
 
 @RequestScoped
@@ -14,17 +14,8 @@ import br.com.pedido.service.ProductService;
 public class ProductRequestController {
 	@Inject
 	ProductService productService;
-	
-	@PostConstruct
-	public void init(){
-		System.out.println(" Bean executado2! ");
-	}
-	
-	public String getMessage(){
-		return "Hello World JSF!!";
-	}
-	
-	public List<String> getProducts() {
+
+	public List<Product> getProducts() {
         return productService.getProducts();
 	}
 }
--- src/br/com/pedido/service/ProductService.java
+++ src/br/com/pedido/service/ProductService.java
@@ -3,11 +3,19 @@ package br.com.pedido.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.pedido.entity.Product;
+
 public class ProductService {
-	public List<String> getProducts() {
-		List<String> products = new ArrayList<String>();
-		products.add("produto1a");
-		products.add("produto2b");
+	public List<Product> getProducts() {
+		Product product1 = new Product();
+		product1.setName("ABACAXI CARAMELADO 01 UNIDADE");
+		
+		Product product2 = new Product();
+		product2.setName("ÁGUA - COM GÁS");
+		
+		List<Product> products = new ArrayList<Product>();
+		products.add(product1);
+		products.add(product2);
 		return products;
 	}
 }
