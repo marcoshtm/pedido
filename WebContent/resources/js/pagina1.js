509ba4443567a3ec4c00c4d3ff30185c7c6fd2f8
Marcos Hartmann <marcos.hartmann@litecode.com.br>
2016-10-21 17:44:30 -0200

    choose product pressing enter on search box

--- WebContent/resources/js/pagina1.js
+++ WebContent/resources/js/pagina1.js
@@ -1,4 +1,32 @@
-function globalSearch(widget) {
-    $(PF(widget).jqId + "\\:globalFilter").val(PF('searchText').jq.val());
-    $(PF(widget).jqId + "\\:globalFilter").trigger("keyup");
+$(function() {	
+	$('form').keypress(function(e) {
+        if (e.keyCode == 13) {
+            return false;
+         }
+	})
+	
+	$('.search-input').keyup(function(e){
+		e.preventDefault();
+		var code = e.which;
+	    if(code==13) {
+	    	onEnter();
+		} else {
+			productSearch();
+		}
+	})
+});
+
+function productSearch() {
+    $(PF('productTable').jqId + "\\:globalFilter").val(PF('searchText').jq.val());
+    $(PF('productTable').jqId + "\\:globalFilter").trigger("keyup");
+}
+
+function onEnter() {
+	var products = $('.product-name').length;
+	if (products == 1) {
+		var product = $('.product-name');
+		PF('productTable').clearFilters();
+		product.click();
+		$('.search-input').val('');
+	}
 }
--- src/br/com/pedido/domain/order/OrderMock.java
+++ src/br/com/pedido/domain/order/OrderMock.java
@@ -24,43 +24,4 @@ public class OrderMock {
 		customer.setPhoneNumber("99667012");
 		return customer;
 	}
-	
-	public String getOrderJsonMock() {
-		String orderJson = "{"
-				+"  \"id_loja\":1,"
-				+"	\"authkey\":\"hello123\","
-				+"	\"id_pedido\":\"101\","
-				+"	\"dthr_criacao\":\"101120161615\","
-				+"	\"previsao_entrega\":\"111120161715\","
-				+"	\"total\":10.6,"
-				+"	\"frete\":2,"
-				+"	\"pagamento\":{"
-				+"		\"pagamento_status\":\"nao_pago\","
-				+"		\"pagamento_forma\":\"dinheiro\""
-				+"	},"
-				+"	\"itens\":["
-				+"		{"
-				+"			\"preco\":10.9,"
-				+"			\"quantidade\":1,"
-				+"			\"sku\":\"901\""
-				+"		}, {"
-				+"			\"preco\":10,"
-				+"			\"quantidade\":1,"
-				+"			\"sku\":\"902\""
-				+"		}],"
-				+"	\"cliente\":{"
-				+"		\"endereco_rua\":\"Rua Um\","
-				+"		\"endereco_numero\":\"100\","
-				+"		\"endereco_cidade\":\"Porto Alegre\","
-				+"		\"endereco_uf\":\"RS\","
-				+"		\"endereco_bairro\":\"Centro\","
-				+"     	\"endereco_complemento\":\"20\","
-				+"       \"endereco_cep\":\"90820180\","
-				+"       \"cliente_nome\":\"Luiza Nunes\","
-				+"       \"cliente_celular\":\"99667012\""
-				+"	}"
-				+"}";
-		
-		return orderJson;
-	}
 }
